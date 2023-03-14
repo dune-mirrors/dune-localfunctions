ae89a97d3efb43c35a5a91180c1cf7d8a23dcb88
Simon Praetorius <simon.praetorius@tu-dresden.de>
2023-03-14 18:51:10 +0100

    Add copyright header

--- dune/localfunctions/lobatto/generate.py
+++ dune/localfunctions/lobatto/generate.py
@@ -1,3 +1,5 @@
+# SPDX-FileCopyrightText: Copyright © DUNE Project contributors, see file LICENSE.md in module root
+# SPDX-License-Identifier: LicenseRef-GPL-2.0-only-with-DUNE-exception
 from sympy import *
 import numpy as np
 
